813ff6010804f032baa0f69d9374f31937a3d7f2
alba620 <alba620@gmail.com>
2022-03-30 13:38:14 +0200

    #9 basic features of houses

--- house-scraper/scraper/scrapers/fotocasa.py
+++ house-scraper/scraper/scrapers/fotocasa.py
@@ -13,6 +13,62 @@ class FotocasaScraper(HouseScraper):
 
     __urls = dict()
 
+    def _scrape_house_page(self, location: str, url : str) -> dict: 
+        """
+        Scrapes a certain house detail page
+
+        Parameters
+        ----------
+        location: str
+            Location the house in in
+        url : str
+            URL to scrap
+        
+        Returns
+        -------
+        Dictionary with all the info on the house
+        """
+        with utils.get_selenium() as driver:
+            try:
+                house = {'id': '', 'url': '', 'title': '', 'location': '', 'sublocation': '',
+                    'price': '', 'm2': '', 'rooms': '', 'floor': '', 'photos': '', 'map': '',
+                    'view3d': '', 'video': '', 'home-staging': '', 'description': ''}
+                utils.log(f'[fotocasa:{location}] Scraping {url}')
+                utils.mini_wait()
+                driver.get(url)
+                main_content = self.try_page(driver, lambda: driver.find_element(by=By.CSS_SELECTOR, value='#App > div.re-Page > main > div.re-LayoutContainer.re-LayoutContainer--large > div.re-ContentDetail-topContainer'))
+                house['id'] = 1111 #TODO re.match("(\d+)", url)
+                house['url'] = driver.current_url
+                house['title'] = main_content.find_element(by=By.CSS_SELECTOR, value='div > section:nth-child(1) > div > div.re-DetailHeader-header > div.re-DetailHeader-propertyTitleContainer > h1').text
+                house['location'] = location
+                house['sublocation'] = "sublocation"
+                house['price'] = main_content.find_element(by= By.CSS_SELECTOR, value='div > section:nth-child(1) > div > div.re-DetailHeader-header > div.re-DetailHeader-userActionContainer > div.re-DetailHeader-priceContainer > span').text.replace(' €', '')
+                house['description'] = main_content.find_element(by=By.CSS_SELECTOR, value='div > section:nth-child(1) > div > div.fc-DetailDescriptionContainer > div.sui-MoleculeCollapsible.sui-MoleculeCollapsible--withGradient.is-collapsed > div > div > p').text.strip()
+                
+                house['m2'] =  main_content.find_element(by=By.CSS_SELECTOR, value='div > section:nth-child(1) > div > div.re-DetailHeader-header > div.re-DetailHeader-propertyTitleContainer > ul > li:nth-child(3) > span:nth-child(2) > span').text
+                house['rooms'] = main_content.find_element(by=By.CSS_SELECTOR, value='div > section:nth-child(1) > div > div.re-DetailHeader-header > div.re-DetailHeader-propertyTitleContainer > ul > li:nth-child(1) > span:nth-child(2) > span').text
+                house['floor'] = main_content.find_element(by=By.CSS_SELECTOR, value='div > section:nth-child(1) > div > div.re-DetailHeader-header > div.re-DetailHeader-propertyTitleContainer > ul > li:nth-child(4) > span:nth-child(2) > span').text
+                house['baths'] = main_content.find_element(by=By.CSS_SELECTOR, value='div > section:nth-child(1) > div > div.re-DetailHeader-header > div.re-DetailHeader-propertyTitleContainer > ul > li:nth-child(2) > span:nth-child(2) > span').text
+                #house['others'] = main_content.find_element(by=By.CSS_SELECTOR, value='div > section:nth-child(1) > div > div.re-DetailHeader-header > div.re-DetailHeader-propertyTitleContainer > ul > li:nth-child(2) > span:nth-child(2) > span')
+                
+
+
+                '''
+                anchors = main_content.find_element(by=By.CSS_SELECTOR, value='div.fake-anchors')
+                features = main_content.find_element(by=By.CSS_SELECTOR, value='div.info-features')
+                extended_features = main_content.find_element(by=By.CSS_SELECTOR, value='section#details > div.details-property')
+                house = self._get_house_features(driver, anchors, features, extended_features, house)
+                
+                
+                '''
+                utils.mini_wait()
+                return house
+            except NoSuchElementException as e:
+                utils.error(f'[{self.id}] Something happened!')
+                utils.error(f'Exception: {e.msg}')
+                return None
+    
+
     def _scrape_houses_details(self,location, houses_list) -> list:
         houses = list()
         with ThreadPoolExecutor(max_workers=config.MAX_WORKERS) as executor:
@@ -21,8 +77,7 @@ class FotocasaScraper(HouseScraper):
             house_number = 0
             #for url, location in self.__urls.items():
             for house_url in houses_list:
-                print(house_url)
-                #futures.append(executor.submit(self._scrape_house_page, location, house_url))
+                futures.append(executor.submit(self._scrape_house_page, location, house_url))
                 house_number = house_number+1
                 if house_number >= max_houses_before_wait:
                     house_number = 0
@@ -34,11 +89,12 @@ class FotocasaScraper(HouseScraper):
 
     def try_page(self, driver, fn):
         madeit = False
-        while not madeit:
+        while not madeit:   
             try:
                 result = fn()
                 madeit = True
             except NoSuchElementException:
+                utils.error(NoSuchElementException)
                 madeit=False
         return result
 
@@ -65,7 +121,6 @@ class FotocasaScraper(HouseScraper):
         while True:
             # see how much we have to wait
             utils.mini_wait() if utils.flip_coin() else utils.wait()
-            print("______________________")
             # browse all pages
             main_content = self.try_page(driver, lambda : driver.find_element(by=By.CSS_SELECTOR, 
                     value='#App > div.re-Page > div.re-SearchPage > main > div > div.re-SearchResult-wrapper > section'))
@@ -74,12 +129,14 @@ class FotocasaScraper(HouseScraper):
             print(f"numero de articulos {len(articles)}")
             for article in articles:
                 # get each article url
-                house_urls = article.find_element(by=By.CSS_SELECTOR, 
-                    value='a.re-CardPackPremium-carousel').get_attribute('href')
-                print(f"found article url: {house_urls}")
-                houses_to_visit.append(house_urls)
-                self.__urls.update(dict.fromkeys(house_urls, location))
-
+                try:
+                    house_urls = article.find_element(by=By.CSS_SELECTOR, 
+                        value='a.re-CardPackPremium-carousel').get_attribute('href')
+                    print(f"found article url: {house_urls}")
+                    houses_to_visit.append(house_urls)
+                    self.__urls.update(dict.fromkeys(house_urls, location))
+                except Exception as e:
+                    utils.error(e)
                 #
             try:
                 next_page = main_content.find_element(by=By.CSS_SELECTOR, value='App > div.re-Page > div.re-SearchPage > main > div > div.re-Pagination > ul > li:nth-child(7) > a')
@@ -111,7 +168,6 @@ class FotocasaScraper(HouseScraper):
             location = "fuenlabrada"
             url_call = f"{config.FOTOCASA_URL}comprar/viviendas/{location}/todas-las-zonas/l?sortType=scoring"
             houses_list = self._scrape_navigation(url_call, location)
-            print(houses_list)
 
             houses = self._scrape_houses_details(location, houses_list)
 
