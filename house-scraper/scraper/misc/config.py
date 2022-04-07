09927d96ee078c2f518569e05532aea64419ea56
alba620 <alba620@gmail.com>
2022-04-07 18:48:59 +0200

    #11 change directory config with location

--- house-scraper/scraper/misc/config.py
+++ house-scraper/scraper/misc/config.py
@@ -24,10 +24,10 @@ IDEALISTA_TMP = os.path.join(TMP_DIR, IDEALISTA_ID)
 FOTOCASA_ID = 'fotocasa'
 FOTOCASA_URL = 'https://www.fotocasa.es/es/'
 FOTOCASA_IMG_DIR = os.path.join(DATASET_DIR, FOTOCASA_ID+'-imgs')   # Directory to save downloaded images
-FOTOCASA_FILE = os.path.join(DATASET_DIR, FOTOCASA_ID + '.csv')
+FOTOCASA_FILE = os.path.join(DATASET_DIR, FOTOCASA_ID)
 FOTOCASA_SCROLL_LOCATION_PAGE = 20                                  # number of iteractions for scrolling in location pages
 FOTOCASA_SCROLL_HOUSE_PAGE = 3                                      # number of iteractions for scrolling in house pages
-FOTOCASA_NUM_PAGES_TO_READ = 1                                      # number of pages to process
+FOTOCASA_NUM_PAGES_TO_READ = 5                                      # number of pages to process
 
 MERGED_FILE = os.path.join(DATASET_DIR, 'merged.csv')               # name of merged dataframe
 MERGED_ZIP_FILE = os.path.join(DATASET_DIR, 'merged.zip')           # name of zip merged dataframe
